7a71f2106db2e4687c84cd7b78f2f90b36b5605d
GustavoBecerraB <78499956+GustavoBecerraB@users.noreply.github.com>
2021-05-11 19:40:00 -0500

    Pregunta D_Completed

--- code/Less_Demands.R
+++ code/Less_Demands.R
@@ -1,4 +1,8 @@
+hotel_limpio <- read.csv(local_host(), header = TRUE, stringsAsFactors = FALSE)
+view(hotel_limpio)
+library(ggplot2)
 table(hotel_limpio$arrival_date_month)
 View(table(hotel_limpio$arrival_date_month))
-hl_months <- table(hotel_limpio$arrival_date_month)
-barplot(hl_months, main = "Distribución mensual de demanda de reserva")
+hl_months <- data.frame(Months = table(hotel_limpio$arrival_date_month))
+hl_months <- hl_months[order(hl_months$Months.Freq),]
+barplot(hl_months$Months.Freq, main = "Distribución mensual de demanda de reserva", names.arg = hl_months$Months.Var1)
