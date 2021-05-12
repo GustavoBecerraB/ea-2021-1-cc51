a84990230b12534d49ce4ea7d58ef2779f7a2a54
GustavoBecerraB <78499956+GustavoBecerraB@users.noreply.github.com>
2021-05-11 19:40:51 -0500

    Pregunta F_Completed

--- code/Required_car_parking_spaces.R
+++ code/Required_car_parking_spaces.R
@@ -1,3 +1,7 @@
+hotel_limpio <- read.csv(local_host(), header = TRUE, stringsAsFactors = FALSE)
+view(hotel_limpio)
+library(ggplot2)
 table(hotel_limpio$required_car_parking_spaces)
 grafico_cp <- table(hotel_limpio$required_car_parking_spaces)
+options(scipen = 999)
 barplot(grafico_cp, main = "Cantidad de estacionamientos requeridos por Reserva")
